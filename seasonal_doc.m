8d25b96495c1ee41093bad3ff290155d23fe6106
Matthew Shultz <matthew.shultz@yale.edu>
2015-02-17 07:31:11 -0500

    Some work looking at Petes seasonal DOC data

---
+++ seasonal_doc.m
@@ -0,0 +1,42 @@
+data =  csvread('../seasonal DOC/seasonal_DOC_proxy/Sheet1-Table 1.csv');
+figure; plot(data(1:end,1), data(1:end,3))
+
+days = data(1:end,1);
+seasonal_doc = data(1:end,3);
+
+interpolated = zeros(365, 1);
+interpolated(1) = seasonal_doc(1);
+for(i = 2:365 )
+    past = find(days < i);
+    future = find(days >= i);
+    
+    previous_index = past(end);
+    next_index = future(1);
+    
+    i
+    before = days(previous_index)
+    after = days(next_index)
+    
+    % amount to interpolate
+    current = i - before;
+    
+    doc_previous = seasonal_doc(previous_index);
+    doc_next = seasonal_doc(next_index);
+    
+    interpolated(i) = doc_previous +  (doc_next - doc_previous) / (after - before) * current;
+end
+interpolated
+
+figure; plot(interpolated)
+
+figure; plotyy(1:365, interpolated, data(1:end,1), data(1:end,3));
+
+num = 365;
+figure;
+plotyy(1:num, interpolated(1:num), data(1:end,1), data(1:end,3));
+
+output = [transpose(1:365), interpolated];
+output
+csvwrite('prepared_seasonal_doc.csv', output);
+
+
