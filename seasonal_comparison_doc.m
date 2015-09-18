9043a88dd53345cb9300c2974714ba19f5d22670
PatchDynamics <xone@fromthegut.org>
2015-09-18 16:30:41 -0400

    Comparing seasonal mass flow

---
+++ seasonal_comparison_doc.m
@@ -0,0 +1,53 @@
+
+spring = month(hf.usgs_timeseries_timestamps) < 6;
+summer = (month(hf.usgs_timeseries_timestamps) >= 6) & (month(hf.usgs_timeseries_timestamps) <= 8);
+fall = month(hf.usgs_timeseries_timestamps) > 8;
+
+figure;
+subplot(2,1,1)
+hold on;
+plot(hf.usgs_timeseries_filtered_discharge(spring), hf.usgs_timeseries_filtered_doc_concentration(spring));
+plot(hf.usgs_timeseries_filtered_discharge(summer), hf.usgs_timeseries_filtered_doc_concentration(summer));
+plot(hf.usgs_timeseries_filtered_discharge(fall), hf.usgs_timeseries_filtered_doc_concentration(fall));
+legend('spring', 'summer', 'fall')
+hold off;
+
+y = hf.usgs_timeseries_filtered_doc_mass_flow;
+subplot(2,1,2)
+hold on;
+plot(hf.usgs_timeseries_filtered_discharge(spring), y(spring));
+plot(hf.usgs_timeseries_filtered_discharge(summer), y(summer));
+plot(hf.usgs_timeseries_filtered_discharge(fall), y(fall));
+legend('spring', 'summer', 'fall')
+hold off;
+
+
+
+
+figure;
+subplot(2,1,1)
+hold on;
+plot(hf.usgs_timeseries_filtered_discharge(spring), hf.usgs_timeseries_filtered_doc_concentration(spring));
+legend('fall')
+hold off;
+
+y = hf.usgs_timeseries_filtered_doc_mass_flow;
+subplot(2,1,2)
+hold on;
+plot(hf.usgs_timeseries_filtered_discharge(fall), y(fall));
+legend('fall')
+hold off;
+
+
+
+figure;
+y = hf.usgs_timeseries_filtered_doc_mass_flow;
+hold on;
+plot(hf.usgs_timeseries_filtered_discharge(fall), y(fall));
+legend('fall')
+hold off;
+
+
+figure;
+plotyy(hf.usgs_timeseries_timestamps, hf.usgs_timeseries_filtered_discharge, hf.usgs_timeseries_timestamps, hf.usgs_timeseries_filtered_doc_mass_flow);
+
